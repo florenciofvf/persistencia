0f252c0491741ba168ab90714b1971e575fce53b
Florêncio Vieira Filho <florenciovieira@gmail.com>
2018-07-07 09:11:30 -0300

    Atualização

--- src/br/com/persist/util/Constantes.java
+++ src/br/com/persist/util/Constantes.java
@@ -1,6 +1,7 @@
 package br.com.persist.util;
 
 public class Constantes {
+	public static final String ENCODING = "UTF-8";
 	public static final String DRIVER = "driver";
 	public static final String LOGIN = "login";
 	public static final String SENHA = "senha";
---
+++ src/br/com/persist/util/XMLUtil.java
@@ -0,0 +1,82 @@
+package br.com.persist.util;
+
+import java.io.File;
+import java.io.PrintWriter;
+
+public class XMLUtil {
+	private final String QL = "\r\n";
+	private final PrintWriter pw;
+	private int tab = -1;
+
+	public XMLUtil(File file) throws Exception {
+		pw = new PrintWriter(file, Constantes.ENCODING);
+	}
+
+	public XMLUtil prologo() {
+		return print("<?xml").atributo("version", 1.0).atributo("encoding", Constantes.ENCODING).print("?>").ql().ql();
+	}
+
+	public XMLUtil atributo(String nome, boolean valor) {
+		return print(" " + nome + "=" + citar("" + valor));
+	}
+
+	public XMLUtil atributo(String nome, double valor) {
+		return print(" " + nome + "=" + citar("" + valor));
+	}
+
+	public XMLUtil atributo(String nome, long valor) {
+		return print(" " + nome + "=" + citar("" + valor));
+	}
+
+	public XMLUtil atributo(String nome, String valor) {
+		return print(" " + nome + "=" + citar(valor));
+	}
+
+	public XMLUtil abrirTag(String nome) {
+		tab++;
+		return tabular().print("<" + nome);
+	}
+
+	public XMLUtil fecharTag() {
+		return print(">").ql();
+	}
+
+	public XMLUtil abrirTag2(String nome) {
+		return abrirTag(nome).fecharTag();
+	}
+
+	public XMLUtil finalizarTag(String nome) {
+		tabular().print("</" + nome + ">").ql();
+		tab--;
+		return this;
+	}
+
+	public XMLUtil abrirFinalizarTag(String nome) {
+		return abrirTag2(nome).finalizarTag(nome);
+	}
+
+	private XMLUtil tabular() {
+		for (int i = 0; i < tab; i++) {
+			print("\t");
+		}
+
+		return this;
+	}
+
+	private String citar(String valor) {
+		return "\"" + valor + "\"";
+	}
+
+	private XMLUtil print(String s) {
+		pw.print(s);
+		return this;
+	}
+
+	public XMLUtil ql() {
+		return print(QL);
+	}
+
+	public void close() {
+		pw.close();
+	}
+}
