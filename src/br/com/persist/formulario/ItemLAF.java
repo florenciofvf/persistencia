b0d168c01d2100579c7905bb0fe7972d152ee858
Florêncio Vieira Filho <florenciovieira@gmail.com>
2018-07-18 18:13:32 -0300

    Atualização

--- src/br/com/persist/formulario/ItemLAF.java
+++ src/br/com/persist/formulario/ItemLAF.java
@@ -1,13 +1,13 @@
 package br.com.persist.formulario;
 
+import javax.swing.JRadioButtonMenuItem;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
 
-import br.com.persist.comp.RadioButtonMenuItem;
 import br.com.persist.util.Util;
 
-public class ItemLAF extends RadioButtonMenuItem {
+public class ItemLAF extends JRadioButtonMenuItem {
 	private static final long serialVersionUID = 1L;
 	private final String classe;
 
