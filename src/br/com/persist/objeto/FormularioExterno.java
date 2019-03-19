0366c101050406825a8bd2fb976778d490d5c65d
Florêncio Vieira Filho <florenciovieira@gmail.com>
2019-03-18 22:03:06 -0300

    Atualização

--- src/br/com/persist/objeto/FormularioExterno.java
+++ src/br/com/persist/objeto/FormularioExterno.java
@@ -4,10 +4,18 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import java.util.Vector;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.ActionMap;
+import javax.swing.InputMap;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.KeyStroke;
 
 import br.com.persist.Objeto;
 import br.com.persist.banco.Conexao;
@@ -25,9 +33,29 @@ public class FormularioExterno extends JFrame implements PainelObjetoListener {
 		painelObjeto = new PainelObjeto(this, objeto, g, padrao, buscaAuto);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		setSize(1000, 600);
+		setActionESC();
 		montarLayout();
 		setLocationRelativeTo(frame);
 		setVisible(true);
+	}
+
+	private void setActionESC() {
+		JComponent component = (JComponent) getContentPane();
+
+		InputMap inputMap = component.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "esc");
+
+		Action action = new AbstractAction() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		};
+
+		ActionMap actionMap = component.getActionMap();
+		actionMap.put("esc", action);
 	}
 
 	private void montarLayout() {
