91755184b14ac16b8926845cb3e3ec8dc0c545c8
Florêncio Vieira Filho <florenciovieira@gmail.com>
2019-03-20 19:55:14 -0300

    Atualização

--- src/br/com/persist/formulario/FormularioDesktop.java
+++ src/br/com/persist/formulario/FormularioDesktop.java
@@ -1,23 +1,14 @@
 package br.com.persist.formulario;
 
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-import javax.swing.AbstractAction;
-import javax.swing.Action;
-import javax.swing.ActionMap;
-import javax.swing.InputMap;
-import javax.swing.JComponent;
-import javax.swing.JFrame;
-import javax.swing.KeyStroke;
-
 import br.com.persist.comp.ScrollPane;
+import br.com.persist.objeto.FormularioAbstrato;
 import br.com.persist.util.Mensagens;
 
-public class FormularioDesktop extends JFrame {
+public class FormularioDesktop extends FormularioAbstrato {
 	private static final long serialVersionUID = 1L;
 	private final Desktop desktop;
 
@@ -25,31 +16,10 @@ public class FormularioDesktop extends JFrame {
 		super(Mensagens.getString("label.persistencia"));
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		desktop = new Desktop(formulario, false);
-		setSize(1000, 600);
-		setActionESC();
 		montarLayout();
 		configurar();
 		setLocationRelativeTo(formulario);
 		setVisible(true);
-	}
-
-	private void setActionESC() {
-		JComponent component = (JComponent) getContentPane();
-
-		InputMap inputMap = component.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
-		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "esc");
-
-		Action action = new AbstractAction() {
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				dispose();
-			}
-		};
-
-		ActionMap actionMap = component.getActionMap();
-		actionMap.put("esc", action);
 	}
 
 	private void configurar() {
---
+++ src/br/com/persist/objeto/FormularioAbstrato.java
@@ -0,0 +1,42 @@
+package br.com.persist.objeto;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.ActionMap;
+import javax.swing.InputMap;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.KeyStroke;
+
+public abstract class FormularioAbstrato extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	public FormularioAbstrato(String titulo) {
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setSize(1000, 600);
+		setTitle(titulo);
+		setActionESC();
+	}
+
+	private void setActionESC() {
+		JComponent component = (JComponent) getContentPane();
+
+		InputMap inputMap = component.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "esc");
+
+		Action action = new AbstractAction() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		};
+
+		ActionMap actionMap = component.getActionMap();
+		actionMap.put("esc", action);
+	}
+}
