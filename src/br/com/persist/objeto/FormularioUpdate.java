ab19016b07c34c0dfb40092a5ae72e8ccc805e6f
Florêncio Vieira Filho <florenciovieira@gmail.com>
2019-03-20 20:24:29 -0300

    Atualização

--- src/br/com/persist/objeto/FormularioUpdate.java
+++ src/br/com/persist/objeto/FormularioUpdate.java
@@ -3,24 +3,14 @@ package br.com.persist.objeto;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Frame;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
 import java.util.Map;
 import java.util.Vector;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import javax.swing.AbstractAction;
-import javax.swing.Action;
-import javax.swing.ActionMap;
-import javax.swing.InputMap;
-import javax.swing.JComponent;
-import javax.swing.JFrame;
-import javax.swing.KeyStroke;
-
 import br.com.persist.banco.Conexao;
 import br.com.persist.util.BuscaAuto.Grupo;
 
-public class FormularioUpdate extends JFrame implements PainelObjetoListener {
+public class FormularioUpdate extends FormularioAbstrato implements PainelObjetoListener {
 	private static final long serialVersionUID = 1L;
 	private final PainelUpdate painelUpdate;
 	private PainelObjetoListener listener;
@@ -30,29 +20,7 @@ public class FormularioUpdate extends JFrame implements PainelObjetoListener {
 		super(titulo);
 		this.listener = listener;
 		painelUpdate = new PainelUpdate(this, instrucao, padrao, mapaChaveValor);
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		setSize(1000, 400);
-		setActionESC();
 		montarLayout();
-	}
-
-	private void setActionESC() {
-		JComponent component = (JComponent) getContentPane();
-
-		InputMap inputMap = component.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
-		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "esc");
-
-		Action action = new AbstractAction() {
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				dispose();
-			}
-		};
-
-		ActionMap actionMap = component.getActionMap();
-		actionMap.put("esc", action);
 	}
 
 	private void montarLayout() {
