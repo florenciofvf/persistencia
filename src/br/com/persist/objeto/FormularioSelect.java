2de6b7510106a8eaa17183f41036ea2558bb7c5c
Florêncio Vieira Filho <florenciovieira@gmail.com>
2019-03-20 20:17:12 -0300

    Atualização

--- src/br/com/persist/objeto/FormularioSelect.java
+++ src/br/com/persist/objeto/FormularioSelect.java
@@ -3,56 +3,26 @@ package br.com.persist.objeto;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Frame;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
 import java.util.Vector;
 import java.util.concurrent.atomic.AtomicBoolean;
-
-import javax.swing.AbstractAction;
-import javax.swing.Action;
-import javax.swing.ActionMap;
-import javax.swing.InputMap;
-import javax.swing.JComponent;
-import javax.swing.JFrame;
-import javax.swing.KeyStroke;
 
 import br.com.persist.banco.Conexao;
 import br.com.persist.formulario.Formulario;
 import br.com.persist.util.BuscaAuto.Grupo;
+import br.com.persist.util.Mensagens;
 
-public class FormularioSelect extends JFrame implements PainelObjetoListener {
+public class FormularioSelect extends FormularioAbstrato implements PainelObjetoListener {
 	private static final long serialVersionUID = 1L;
 	private final PainelSelect painelSelect;
 	private final Formulario formulario;
 
 	public FormularioSelect(Formulario formulario, Frame frame, Conexao padrao) {
+		super(Mensagens.getString("label.pesquisa"));
 		this.formulario = formulario;
 		painelSelect = new PainelSelect(this, padrao);
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		setSize(1000, 600);
-		setActionESC();
-		montarLayout();
 		setLocationRelativeTo(frame);
+		montarLayout();
 		setVisible(true);
-	}
-
-	private void setActionESC() {
-		JComponent component = (JComponent) getContentPane();
-
-		InputMap inputMap = component.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
-		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "esc");
-
-		Action action = new AbstractAction() {
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				dispose();
-			}
-		};
-
-		ActionMap actionMap = component.getActionMap();
-		actionMap.put("esc", action);
 	}
 
 	private void montarLayout() {
