b223a894affb0569cf5e7628ad711a47c385393b
Florêncio Vieira Filho <florenciovieira@gmail.com>
2019-03-31 17:49:46 -0300

    Atualização

---
+++ src/br/com/persist/xml/XMLException.java
@@ -0,0 +1,9 @@
+package br.com.persist.xml;
+
+public class XMLException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public XMLException(Throwable cause) {
+		super(cause);
+	}
+}
