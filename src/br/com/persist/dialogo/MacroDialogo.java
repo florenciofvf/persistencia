e5b22da97ef1ad0f67f9b1d455592bc970a97949
Florêncio Vieira Filho <florenciovieira@gmail.com>
2019-04-07 09:51:55 -0300

    Atualização

--- src/br/com/persist/dialogo/MacroDialogo.java
+++ src/br/com/persist/dialogo/MacroDialogo.java
@@ -2,89 +2,39 @@ package br.com.persist.dialogo;
 
 import java.awt.BorderLayout;
 import java.awt.Frame;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-import javax.swing.JList;
-import javax.swing.ListModel;
-import javax.swing.event.ListDataListener;
+import br.com.persist.container.MacroContainer;
+import br.com.persist.util.IJanela;
 
-import br.com.persist.principal.Formulario;
-import br.com.persist.util.Macro;
-
-public class MacroDialogo extends AbstratoDialogo {
+public class MacroDialogo extends AbstratoDialogoTMP implements IJanela {
 	private static final long serialVersionUID = 1L;
-	private static final Logger LOG = Logger.getGlobal();
-	private JList<Macro.Instrucao> lista;
+	private final MacroContainer container;
 
 	public MacroDialogo(Frame frame) {
-		super(frame, "Macro", 0, 0, false);
-		lista = new JList<>(new Modelo());
+		super(frame, "Macro");
+		container = new MacroContainer();
 		montarLayout();
 		pack();
-		setLocationRelativeTo(frame);
 		configurar();
-		setVisible(true);
-	}
-
-	private void configurar() {
-		lista.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyPressed(KeyEvent e) {
-				if (e.getKeyCode() == KeyEvent.VK_D) {
-					int indice = lista.getSelectedIndex();
-
-					if (indice != -1) {
-						Formulario.macro.excluir(indice);
-						lista.setModel(new Modelo());
-
-						if (!Formulario.macro.isEmpty()) {
-							lista.setSelectedIndex(0);
-						}
-					}
-				}
-			}
-		});
-
-		addWindowListener(new WindowAdapter() {
-			@Override
-			public void windowOpened(WindowEvent e) {
-				lista.setSelectedIndex(0);
-			}
-		});
-	}
-
-	private class Modelo implements ListModel<Macro.Instrucao> {
-		@Override
-		public int getSize() {
-			return Formulario.macro.getInstrucoes().size();
-		}
-
-		@Override
-		public br.com.persist.util.Macro.Instrucao getElementAt(int index) {
-			return Formulario.macro.getInstrucoes().get(index);
-		}
-
-		@Override
-		public void addListDataListener(ListDataListener l) {
-			LOG.log(Level.FINEST, "addListDataListener");
-		}
-
-		@Override
-		public void removeListDataListener(ListDataListener l) {
-			LOG.log(Level.FINEST, "removeListDataListener");
-		}
 	}
 
 	private void montarLayout() {
-		add(BorderLayout.CENTER, lista);
+		add(BorderLayout.CENTER, container);
 	}
 
-	protected void processar() {
-		throw new UnsupportedOperationException();
+	@Override
+	public void fechar() {
+		dispose();
+	}
+
+	private void configurar() {
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowOpened(WindowEvent e) {
+				container.getLista().setSelectedIndex(0);
+			}
+		});
 	}
 }
