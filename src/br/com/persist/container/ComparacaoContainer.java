f4acf875d8163590c360ed63fa2f0743c7ab71e6
Florêncio Vieira Filho <florenciovieira@gmail.com>
2019-11-09 19:57:23 -0200

    Atualização

--- src/br/com/persist/container/ComparacaoContainer.java
+++ src/br/com/persist/container/ComparacaoContainer.java
@@ -2,8 +2,12 @@ package br.com.persist.container;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
 import javax.swing.JComponent;
+import javax.swing.JFileChooser;
 
 import br.com.persist.comp.BarraButton;
 import br.com.persist.comp.Button;
@@ -12,6 +16,7 @@ import br.com.persist.comp.Panel;
 import br.com.persist.comp.TextArea;
 import br.com.persist.comp.TextField;
 import br.com.persist.util.IJanela;
+import br.com.persist.util.Util;
 
 public class ComparacaoContainer extends Panel {
 	private static final long serialVersionUID = 1L;
@@ -31,19 +36,25 @@ public class ComparacaoContainer extends Panel {
 		add(BorderLayout.NORTH, toolbar);
 	}
 
-	private class Controle extends Panel {
+	private class Controle extends Panel implements ActionListener {
 		private static final long serialVersionUID = 1L;
+		private Button btnComparar = new Button("label.comparar");
 		private Button btnArquivo1 = new Button("label.procurar");
 		private Button btnArquivo2 = new Button("label.procurar");
 		private Label lblArquivo1 = new Label("label.arquivo1");
 		private Label lblArquivo2 = new Label("label.arquivo2");
 		private TextField txtArquivo1 = new TextField();
 		private TextField txtArquivo2 = new TextField();
+		private Label lblStatus = new Label();
 
 		Controle() {
-			super(new GridLayout(2, 2));
+			super(new GridLayout(3, 1));
 			add(criarPainel(lblArquivo1, txtArquivo1, btnArquivo1));
 			add(criarPainel(lblArquivo2, txtArquivo2, btnArquivo2));
+			add(criarPainel(btnComparar, lblStatus, new Label()));
+			btnArquivo1.addActionListener(this);
+			btnArquivo2.addActionListener(this);
+			btnComparar.addActionListener(this);
 		}
 
 		private Panel criarPainel(JComponent c1, JComponent c2, JComponent c3) {
@@ -53,6 +64,38 @@ public class ComparacaoContainer extends Panel {
 			panel.add(BorderLayout.EAST, c3);
 
 			return panel;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == btnArquivo1) {
+				selecionar(txtArquivo1);
+			} else if (e.getSource() == btnArquivo2) {
+				selecionar(txtArquivo2);
+			} else if (e.getSource() == btnComparar) {
+				// comparar();
+			}
+		}
+
+		private void selecionar(TextField txt) {
+			File file = getSelectedFile();
+
+			if (file == null) {
+				return;
+			}
+
+			txt.setText(file.getAbsolutePath());
+		}
+
+		private File getSelectedFile() {
+			JFileChooser fileChooser = Util.criarFileChooser(null, false);
+			int opcao = fileChooser.showOpenDialog(ComparacaoContainer.this);
+
+			if (opcao == JFileChooser.APPROVE_OPTION) {
+				return fileChooser.getSelectedFile();
+			}
+
+			return null;
 		}
 	}
 }
