ca17810ea8e2a3334e8b9dfa01525f51e4c5dd7f
Florêncio Vieira Filho <florenciovieira@gmail.com>
2019-12-11 07:58:49 -0200

    Atualização

--- src/br/com/persist/container/ObjetoConfigContainer.java
+++ src/br/com/persist/container/ObjetoConfigContainer.java
@@ -555,7 +555,7 @@ public class ObjetoConfigContainer extends Panel {
 		private static final long serialVersionUID = 1L;
 
 		Fichario() {
-			addTab("label.geral", new PanelGeral());
+			addTab("label.geral", new ScrollPane(new PanelGeral()));
 			addTab("label.desc", new PanelDesc());
 			addTab("label.cor", new PanelCor());
 			addTab("label.cor_fonte", new PanelCorFonte());
