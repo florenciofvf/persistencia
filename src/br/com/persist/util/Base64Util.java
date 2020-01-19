2ff9575e2d79afc6a4c118f827dbc8c9c9c1160a
Florêncio Vieira Filho <florenciovieira@gmail.com>
2020-01-19 17:58:50 -0300

    Atualização

---
+++ src/br/com/persist/util/Base64Util.java
@@ -0,0 +1,13 @@
+package br.com.persist.util;
+
+import javax.xml.bind.DatatypeConverter;
+
+public class Base64Util {
+
+	private Base64Util() {
+	}
+
+	public static String criarBase64(String string) {
+		return DatatypeConverter.printBase64Binary(string.getBytes());
+	}
+}
