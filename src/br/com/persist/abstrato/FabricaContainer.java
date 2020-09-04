5519ba5ef874eca36bd69a33d7410f1be6426d0a
Florêncio Vieira Filho <florenciovieira@gmail.com>
2020-09-03 22:15:31 -0300

    Atualização

--- src/br/com/persist/abstrato/FabricaContainer.java
+++ src/br/com/persist/abstrato/FabricaContainer.java
@@ -8,9 +8,9 @@ import br.com.persist.fichario.PaginaServico;
 import br.com.persist.principal.Formulario;
 
 public interface FabricaContainer {
-	public List<Servico> getServicos(Formulario formulario);
-
 	public List<JMenuItem> criarMenuItens(Formulario formulario);
+
+	public List<Servico> getServicos(Formulario formulario);
 
 	public PaginaServico getPaginaServico();
 }
