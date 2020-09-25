6ac68ade331bb662e3c8cf9a07451ab9933c3820
Florêncio Vieira Filho <florenciovieira@gmail.com>
2020-09-25 08:43:42 -0300

    Revert "Revert "Revert "Atulização"""
    This reverts commit 16fac6bb2b2efb04d6812da64187fa4bc1619c0b.

--- src/br/com/persist/main/Main.java
+++ src/br/com/persist/main/Main.java
@@ -82,6 +82,7 @@ public class Main {
 			if (files != null) {
 				for (File f : files) {
 					String s = f.getName().toLowerCase();
+
 					if (s.endsWith(".jar")) {
 						URI uri = f.toURI();
 						urls.add(uri.toURL());
