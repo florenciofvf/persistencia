1a0ff9499c11a2af3dae94bb8996d0ae70b07519
Florencio Vieira Filho <florenciovieira@gmail.com>
2021-08-08 20:12:51 -0300

    Atualização

--- src/br/com/persist/plugins/objeto/ObjetoSuperficie.java
+++ src/br/com/persist/plugins/objeto/ObjetoSuperficie.java
@@ -2318,7 +2318,7 @@ public class ObjetoSuperficie extends Desktop implements ObjetoListener {
 			Referencia ref = new Referencia(grupo, tabela, campo);
 			ref.setVazioInvisivel(true);
 			mapaRef.put("ref", ref);
-			Pesquisa pesquisa = (Pesquisa) mapaRef.get("pesquisa");
+			Pesquisa pesquisa = (Pesquisa) mapaRef.get(ObjetoConstantes.PESQUISA);
 			pesquisa.add(ref);
 		}
 
