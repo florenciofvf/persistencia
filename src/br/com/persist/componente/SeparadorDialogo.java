a6eeed86cbbdfb876d93855e6165f1b263bd8179
Florencio Vieira Filho <florenciovieira@gmail.com>
2021-11-05 10:49:01 -0300

    Atualização

--- src/br/com/persist/componente/SeparadorDialogo.java
+++ src/br/com/persist/componente/SeparadorDialogo.java
@@ -19,15 +19,17 @@ public class SeparadorDialogo extends AbstratoDialogo {
 	private static final long serialVersionUID = 1L;
 	private final SeparadorContainer container;
 
-	private SeparadorDialogo(Frame frame, String titulo, JTable table, int indiceColuna, boolean comAspas) {
+	private SeparadorDialogo(Frame frame, String titulo, JTable table, int indiceColuna, boolean comAspas,
+			List<String> listaString) {
 		super(frame, titulo);
-		container = new SeparadorContainer(this, table, indiceColuna, comAspas);
+		container = new SeparadorContainer(this, table, indiceColuna, comAspas, listaString);
 		montarLayout();
 	}
 
-	private SeparadorDialogo(Dialog dialog, String titulo, JTable table, int indiceColuna, boolean comAspas) {
+	private SeparadorDialogo(Dialog dialog, String titulo, JTable table, int indiceColuna, boolean comAspas,
+			List<String> listaString) {
 		super(dialog, titulo);
-		container = new SeparadorContainer(this, table, indiceColuna, comAspas);
+		container = new SeparadorContainer(this, table, indiceColuna, comAspas, listaString);
 		montarLayout();
 	}
 
@@ -35,15 +37,16 @@ public class SeparadorDialogo extends AbstratoDialogo {
 		add(BorderLayout.CENTER, container);
 	}
 
-	public static void criar(Component c, String titulo, JTable table, int indiceColuna, boolean comAspas) {
+	public static void criar(Component c, String titulo, JTable table, int indiceColuna, boolean comAspas,
+			List<String> listaString) {
 		Component comp = Util.getViewParent(c);
 		SeparadorDialogo form = null;
 		if (comp instanceof Frame) {
-			form = new SeparadorDialogo((Frame) comp, titulo, table, indiceColuna, comAspas);
+			form = new SeparadorDialogo((Frame) comp, titulo, table, indiceColuna, comAspas, listaString);
 		} else if (comp instanceof Dialog) {
-			form = new SeparadorDialogo((Dialog) comp, titulo, table, indiceColuna, comAspas);
+			form = new SeparadorDialogo((Dialog) comp, titulo, table, indiceColuna, comAspas, listaString);
 		} else {
-			form = new SeparadorDialogo((Frame) null, titulo, table, indiceColuna, comAspas);
+			form = new SeparadorDialogo((Frame) null, titulo, table, indiceColuna, comAspas, listaString);
 		}
 		form.pack();
 		form.setLocationRelativeTo(comp != null ? comp : c);
@@ -61,13 +64,16 @@ class SeparadorContainer extends Panel {
 	private final CheckBox chkQuebraLinha = new CheckBox("label.quebrarLinha");
 	private final Button btnCopiar = new Button("label.copiar");
 	private final TextField txtSeparador = new TextField();
+	private final List<String> listaString;
 	private final boolean comAspas;
 	private final int indiceColuna;
 	private final Dialog dialogo;
 	private final JTable table;
 
-	public SeparadorContainer(Dialog dialogo, JTable table, int indiceColuna, boolean comAspas) {
+	public SeparadorContainer(Dialog dialogo, JTable table, int indiceColuna, boolean comAspas,
+			List<String> listaString) {
 		this.indiceColuna = indiceColuna;
+		this.listaString = listaString;
 		this.comAspas = comAspas;
 		this.dialogo = dialogo;
 		this.table = table;
@@ -89,14 +95,14 @@ class SeparadorContainer extends Panel {
 	}
 
 	private void copiar() {
-		List<String> lista = Util.getValoresLinha(table, indiceColuna);
+		List<String> lista = table != null ? Util.getValoresLinha(table, indiceColuna) : listaString;
 		String string = Util.getStringLista(lista, txtSeparador.getText(), chkQuebraLinha.isSelected(), comAspas);
 		Util.setContentTransfered(string);
 		dialogo.dispose();
 	}
 
 	public void checarCopias() {
-		List<String> lista = Util.getValoresLinha(table, indiceColuna);
+		List<String> lista = table != null ? Util.getValoresLinha(table, indiceColuna) : listaString;
 		if (lista.size() == 1) {
 			String string = Util.getStringLista(lista, Constantes.VAZIO, false, comAspas);
 			Util.setContentTransfered(string);
