d8617d2eea3aa8bf0588ee18b098cefe62af7e5c
Florencio Vieira Filho <florenciovieira@gmail.com>
2022-01-28 16:18:40 -0300

    Atualização

--- src/br/com/persist/plugins/requisicao/conteudo/ConteudoHTML.java
+++ src/br/com/persist/plugins/requisicao/conteudo/ConteudoHTML.java
@@ -2,9 +2,13 @@ package br.com.persist.plugins.requisicao.conteudo;
 
 import java.awt.Component;
 import java.io.InputStream;
+import java.net.URL;
 
 import javax.swing.Icon;
+import javax.swing.JEditorPane;
 import javax.swing.JTextPane;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
 
 import br.com.persist.assistencia.Icones;
 import br.com.persist.assistencia.Util;
@@ -15,11 +19,39 @@ public class ConteudoHTML extends RequisicaoHeader {
 	@Override
 	public Component exibir(InputStream is) throws Exception {
 		JTextPane area = new JTextPane();
+		area.setEditable(false);
+		area.addHyperlinkListener(new Listener());
 		area.setContentType("text/html");
 		String string = Util.getString(is);
 		area.setText(string);
 		setAuthToken(string);
 		return new ScrollPane(area);
+	}
+
+	private class Listener implements HyperlinkListener {
+
+		public void hyperlinkUpdate(HyperlinkEvent e) {
+			if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+				JEditorPane pane = (JEditorPane) e.getSource();
+				URL url = e.getURL();
+				String desc = e.getDescription();
+				if (url == null && !Util.estaVazio(desc)) {
+					Object obj = Util.getValorInputDialog(null, "label.atencao", "Complete a URL", desc);
+					if (obj != null) {
+						try {
+							url = new URL(obj.toString());
+						} catch (Exception ex) {
+							pane.setText("Erro: " + ex.getMessage());
+						}
+					}
+				}
+				try {
+					pane.setPage(url);
+				} catch (Exception ex) {
+					pane.setText("Erro: " + ex.getMessage());
+				}
+			}
+		}
 	}
 
 	@Override
