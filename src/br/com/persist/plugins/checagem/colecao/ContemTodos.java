d337da276e941b246b17078f41877db20485143c
Florencio Vieira Filho <florenciovieira@gmail.com>
2022-06-19 16:45:04 -0300

    Atualização

---
+++ src/br/com/persist/plugins/checagem/colecao/ContemTodos.java
@@ -0,0 +1,22 @@
+package br.com.persist.plugins.checagem.colecao;
+
+import java.util.Collection;
+
+import br.com.persist.plugins.checagem.ChecagemException;
+import br.com.persist.plugins.checagem.Contexto;
+import br.com.persist.plugins.checagem.FuncaoBinaria;
+
+public class ContemTodos extends FuncaoBinaria {
+	private static final String ERRO = "Erro Contem";
+
+	@Override
+	public Object executar(Contexto ctx) throws ChecagemException {
+		Object op0 = param0().executar(ctx);
+		Object op1 = param1().executar(ctx);
+		checkObrigatorioCollection(op0, ERRO + " >>> op0");
+		checkObrigatorioCollection(op1, ERRO + " >>> op1");
+		Collection<?> colecao = (Collection<?>) op0;
+		Collection<?> colecao2 = (Collection<?>) op1;
+		return colecao.containsAll(colecao2);
+	}
+}
---
+++ src/br/com/persist/plugins/checagem/colecao/CriarSet.java
@@ -0,0 +1,21 @@
+package br.com.persist.plugins.checagem.colecao;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import br.com.persist.plugins.checagem.ChecagemException;
+import br.com.persist.plugins.checagem.Contexto;
+import br.com.persist.plugins.checagem.FuncaoVaziaOuNParam;
+import br.com.persist.plugins.checagem.Sentenca;
+
+public class CriarSet extends FuncaoVaziaOuNParam {
+
+	@Override
+	public Object executar(Contexto ctx) throws ChecagemException {
+		Set<Object> resposta = new HashSet<>();
+		for (Sentenca s : parametros) {
+			resposta.add(s.executar(ctx));
+		}
+		return resposta;
+	}
+}
