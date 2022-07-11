f56a470f078ce6527789c292dde3a5cfb2edf3db
Florencio Vieira Filho <florenciovieira@gmail.com>
2022-07-11 09:08:09 -0300

    Atualização

--- src/br/com/persist/plugins/checagem/util/ExecModulo.java
+++ src/br/com/persist/plugins/checagem/util/ExecModulo.java
@@ -9,23 +9,25 @@ import br.com.persist.plugins.checagem.Checagem;
 import br.com.persist.plugins.checagem.ChecagemException;
 import br.com.persist.plugins.checagem.ChecagemUtil;
 import br.com.persist.plugins.checagem.Contexto;
-import br.com.persist.plugins.checagem.FuncaoUnaria;
+import br.com.persist.plugins.checagem.FuncaoBinaria;
 import br.com.persist.plugins.checagem.Modulo;
 
-public class ExecModulo extends FuncaoUnaria {
+public class ExecModulo extends FuncaoBinaria {
 	private static final String ERRO = "Erro ExecModulo >>> ";
 
 	@Override
 	public Object executar(Checagem checagem, Bloco bloco, Contexto ctx) throws ChecagemException {
 		Object op0 = param0().executar(checagem, bloco, ctx);
 		checkObrigatorioString(op0, ERRO + "op0");
+		Object op1 = parametros.get(1).executar(checagem, bloco, ctx);
+		checkObrigatorioMap(op1, ERRO + "op1");
 		String idModulo = (String) op0;
 		ChecagemUtil.checarModulo(idModulo);
 		Modulo modulo = checagem.getModulo(idModulo);
 		if (modulo == null) {
 			throwModuloInexistente(idModulo);
 		}
-		List<Object> lista = modulo.executar(checagem, null, ctx);
+		List<Object> lista = modulo.executar(checagem, null, Contexto.criar(op1));
 		StringBuilder sb = new StringBuilder();
 		for (Object object : lista) {
 			append(sb, object);
