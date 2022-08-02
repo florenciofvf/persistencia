1c5f135d229d732aaf313011abe2693547c682c3
Florencio Vieira Filho <florenciovieira@gmail.com>
2022-08-02 05:41:01 -0300

    Atualização

--- src/br/com/persist/plugins/checagem/ChecagemGramatica.java
+++ src/br/com/persist/plugins/checagem/ChecagemGramatica.java
@@ -9,11 +9,43 @@ import java.util.List;
 import java.util.Map;
 
 import br.com.persist.assistencia.Util;
+import br.com.persist.plugins.checagem.comparacao.Igual;
+import br.com.persist.plugins.checagem.comparacao.Maior;
+import br.com.persist.plugins.checagem.comparacao.MaiorIgual;
+import br.com.persist.plugins.checagem.comparacao.Menor;
+import br.com.persist.plugins.checagem.comparacao.MenorIgual;
+import br.com.persist.plugins.checagem.logico.E;
+import br.com.persist.plugins.checagem.logico.Ou;
+import br.com.persist.plugins.checagem.logico.Oux;
+import br.com.persist.plugins.checagem.matematico.Dividir;
+import br.com.persist.plugins.checagem.matematico.Multiplicar;
+import br.com.persist.plugins.checagem.matematico.Resto;
+import br.com.persist.plugins.checagem.matematico.Somar;
+import br.com.persist.plugins.checagem.matematico.Subtrair;
 
 public class ChecagemGramatica {
-	static Map<String, String> map = new HashMap<>();
+	static final Map<String, String> prefixas = new HashMap<>();
+	static final Map<String, String> infixas = new HashMap<>();
 
 	private ChecagemGramatica() {
+	}
+
+	static {
+		infixas.put("+", Somar.class.getName());
+		infixas.put("-", Subtrair.class.getName());
+		infixas.put("*", Multiplicar.class.getName());
+		infixas.put("/", Dividir.class.getName());
+		infixas.put("%", Resto.class.getName());
+
+		infixas.put("=", Igual.class.getName());
+		infixas.put("<", Menor.class.getName());
+		infixas.put("<=", MenorIgual.class.getName());
+		infixas.put(">", Maior.class.getName());
+		infixas.put(">=", MaiorIgual.class.getName());
+
+		infixas.put("^", Oux.class.getName());
+		infixas.put("&&", E.class.getName());
+		infixas.put("||", Ou.class.getName());
 	}
 
 	static void mapear(String arquivo) throws IOException, ClassNotFoundException {
@@ -26,7 +58,7 @@ public class ChecagemGramatica {
 						chave = string.toLowerCase().trim();
 					} else {
 						Class.forName(string.trim());
-						map.put(chave, string.trim());
+						prefixas.put(chave, string.trim());
 						chave = null;
 					}
 				}
@@ -46,9 +78,12 @@ public class ChecagemGramatica {
 		SentencaRaiz sentencaRaiz = new SentencaRaiz();
 		List<Token> tokens = checagemToken.getTokens();
 		TipoFuncao funcaoSelecionada = sentencaRaiz;
-		for (int i = 0; i < tokens.size(); i++) {
-			Token token = tokens.get(i);
-			if (token.isParenteseIni()) {
+		for (Token token : tokens) {
+			if (token.isFuncaoPrefixa()) {
+				//
+			} else if (token.isFuncaoInfixa()) {
+				//
+			} else if (token.isParenteseIni()) {
 				TipoAtomico atomico = (TipoAtomico) funcaoSelecionada.getUltimoParametro();
 				TipoFuncao funcao = transformarEmFuncao(atomico);
 				funcaoSelecionada.setUltimoParametro(funcao);
@@ -56,16 +91,18 @@ public class ChecagemGramatica {
 			} else if (token.isParenteseFim()) {
 				funcaoSelecionada.encerrar();
 				funcaoSelecionada = (TipoFuncao) funcaoSelecionada.getPai();
-			} else if (token.isFuncaoInfixa()) {
-			} else if (token.isAleatorios()) {
+			} else if (token.isVariavel()) {
+				//
 			} else if (token.isVirgula()) {
 				funcaoSelecionada.preParametro();
 			} else if (token.isBoolean() || token.isString() || token.isDouble() || token.isLong()) {
+				//
 			} else {
 				TipoAtomico atomico = Token.criarTipoAtomico(token);
 				funcaoSelecionada.addParam(atomico);
 			}
 		}
+
 		if (sentencaRaiz.getSentenca() == null) {
 			throw new ChecagemException(ChecagemGramatica.class, "Sentenca invalida (vazia!) >>> " + bloco);
 		}
@@ -75,11 +112,14 @@ public class ChecagemGramatica {
 		bloco.setSentenca(sentencaRaiz.getSentenca());
 	}
 
-	private static TipoFuncao transformarEmFuncao(TipoAtomico atomico) throws ChecagemException {
-		String chave = atomico.getValorString().toLowerCase().trim();
-		String classe = map.get(chave);
+	private static TipoFuncao criarFuncao(Token token) throws ChecagemException {
+		if(!token.isFuncaoPrefixa()) {
+			throw new ChecagemException(ChecagemGramatica.class, "Nao eh funcao prefixa >>> " + token);
+		}
+		String chave = token.getValor().toString().toLowerCase();
+		String classe = prefixas.get(chave);
 		if (classe == null) {
-			return new FuncaoPadrao();
+			throw new ChecagemException(ChecagemGramatica.class, token.getIndice() + " <<< Funcao nao declarada >>> " + chave);
 		}
 		Class<?> klass = null;
 		try {
