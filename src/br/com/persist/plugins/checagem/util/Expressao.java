3e147bfb907764a2501349846dd441056bbe310a
Florencio Vieira Filho <florenciovieira@gmail.com>
2022-08-07 09:14:54 -0300

    Atualização

--- src/br/com/persist/plugins/checagem/util/Expressao.java
+++ src/br/com/persist/plugins/checagem/util/Expressao.java
@@ -1,14 +1,41 @@
 package br.com.persist.plugins.checagem.util;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 import br.com.persist.plugins.checagem.Bloco;
 import br.com.persist.plugins.checagem.Checagem;
 import br.com.persist.plugins.checagem.ChecagemException;
+import br.com.persist.plugins.checagem.ChecagemNumero;
 import br.com.persist.plugins.checagem.Contexto;
 import br.com.persist.plugins.checagem.funcao.FuncaoUnaria;
 
-public class Expressao extends FuncaoUnaria {
+public class Expressao extends FuncaoUnaria implements ChecagemNumero {
+	private boolean negarExpressao;
+
 	@Override
 	public Object executar(Checagem checagem, Bloco bloco, Contexto ctx) throws ChecagemException {
-		return param0().executar(checagem, bloco, ctx);
+		Object obj = param0().executar(checagem, bloco, ctx);
+		if (negarExpressao) {
+			if (ehInteiro(obj)) {
+				return getNativoInteiro(obj) * -1;
+			} else if (ehFlutuante(obj)) {
+				return getNativoFlutuante(obj) * -1;
+			} else if (ehBigInteger(obj)) {
+				return getNativoBigInteger(obj).multiply(BigInteger.valueOf(-1));
+			} else if (ehBigDecimal(obj)) {
+				return getNativoBigDecimal(obj).multiply(BigDecimal.valueOf(-1));
+			}
+			throw new ChecagemException(getClass(), "O valor nao pode ser negado >>> " + obj);
+		}
+		return obj;
+	}
+
+	public boolean isNegarExpressao() {
+		return negarExpressao;
+	}
+
+	public void setNegarExpressao(boolean negarExpressao) {
+		this.negarExpressao = negarExpressao;
 	}
 }
