42c60fca2576b7e4c34d1ff2e7aecce70773d520
Florencio Vieira Filho <florenciovieira@gmail.com>
2022-08-08 06:28:28 -0300

    Atualização

--- src/br/com/persist/marca/XMLHandler.java
+++ src/br/com/persist/marca/XMLHandler.java
@@ -1,6 +1,35 @@
 package br.com.persist.marca;
 
+import org.xml.sax.SAXException;
+import org.xml.sax.ext.LexicalHandler;
 import org.xml.sax.helpers.DefaultHandler;
 
-public class XMLHandler extends DefaultHandler {
+public abstract class XMLHandler extends DefaultHandler implements LexicalHandler {
+	@Override
+	public void startDTD(String name, String publicId, String systemId) throws SAXException {
+	}
+
+	@Override
+	public void endDTD() throws SAXException {
+	}
+
+	@Override
+	public void startEntity(String name) throws SAXException {
+	}
+
+	@Override
+	public void endEntity(String name) throws SAXException {
+	}
+
+	@Override
+	public void startCDATA() throws SAXException {
+	}
+
+	@Override
+	public void endCDATA() throws SAXException {
+	}
+
+	@Override
+	public void comment(char[] ch, int start, int length) throws SAXException {
+	}
 }
