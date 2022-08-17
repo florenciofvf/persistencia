c11fc5321b511d01d7df1a74150619ed9b5c95f5
Florencio Vieira Filho <florenciovieira@gmail.com>
2022-08-17 09:36:14 -0300

    Atualização

--- src/br/com/persist/plugins/checagem/ChecagemPagina.java
+++ src/br/com/persist/plugins/checagem/ChecagemPagina.java
@@ -122,6 +122,11 @@ public class ChecagemPagina extends Panel {
 		}
 
 		private void executar() {
+			try {
+				ChecagemUtil.atualizarEstrutura(file, areaParametros.getText());
+			} catch (ChecagemException | XMLException e) {
+				Util.mensagem(ChecagemPagina.this, e.getMessage());
+			}
 			Contexto ctx = new Contexto();
 			String string = ChecagemUtil.executar(file.getName(), null, ctx);
 			areaResultado.setText(string);
