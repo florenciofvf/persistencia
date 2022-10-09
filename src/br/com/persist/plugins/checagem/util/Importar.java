0680ebe91d0420c2ed1efcf515ebbcc6743ad36b
Florencio Vieira Filho <florenciovieira@gmail.com>
2022-10-09 11:44:12 -0300

    Atualização

--- src/br/com/persist/plugins/checagem/util/Importar.java
+++ src/br/com/persist/plugins/checagem/util/Importar.java
@@ -4,9 +4,9 @@ import br.com.persist.plugins.checagem.Bloco;
 import br.com.persist.plugins.checagem.Checagem;
 import br.com.persist.plugins.checagem.ChecagemException;
 import br.com.persist.plugins.checagem.Contexto;
-import br.com.persist.plugins.checagem.funcao.FuncaoUnaria;
+import br.com.persist.plugins.checagem.funcao.FuncaoUnariaOuNParam;
 
-public class Importar extends FuncaoUnaria {
+public class Importar extends FuncaoUnariaOuNParam {
 	private static final String ERRO = "Erro Importar";
 
 	@Override
@@ -14,6 +14,11 @@ public class Importar extends FuncaoUnaria {
 		Object op0 = param0().executar(checagem, bloco, ctx);
 		checkObrigatorioString(op0, ERRO + " >>> op0");
 		checagem.add(op0.toString());
+		for (int i = 1; i < parametros.size(); i++) {
+			Object valor = parametros.get(i).executar(checagem, bloco, ctx);
+			checkObrigatorioString(valor, ERRO + " >>> op" + i);
+			checagem.add(op0.toString());
+		}
 		return op0;
 	}
 }
