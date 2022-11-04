d4645bfe7676bc225fc0f6849dd798661248762a
Florencio Vieira Filho <florenciovieira@gmail.com>
2022-11-04 17:27:11 -0300

    Atualização

--- src/br/com/persist/plugins/checagem/Sentenca.java
+++ src/br/com/persist/plugins/checagem/Sentenca.java
@@ -2,4 +2,6 @@ package br.com.persist.plugins.checagem;
 
 public interface Sentenca {
 	public Object executar(Checagem checagem, Bloco bloco, Contexto ctx) throws ChecagemException;
+
+	public String getDoc();
 }
