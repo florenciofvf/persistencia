b8b82c3e161866e75c964ae588ca9045c90154e9
Florencio Vieira Filho <florenciovieira@gmail.com>
2022-12-19 21:30:43 -0300

    Atualização

--- src/br/com/persist/plugins/anotacao/AnotacaoSplit.java
+++ src/br/com/persist/plugins/anotacao/AnotacaoSplit.java
@@ -1,8 +1,22 @@
 package br.com.persist.plugins.anotacao;
 
+import static br.com.persist.componente.BarraButtonEnum.BAIXAR;
+import static br.com.persist.componente.BarraButtonEnum.COLAR;
+import static br.com.persist.componente.BarraButtonEnum.COPIAR;
+import static br.com.persist.componente.BarraButtonEnum.LIMPAR;
+import static br.com.persist.componente.BarraButtonEnum.SALVAR;
+
+import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Objects;
 
@@ -13,9 +27,17 @@ import br.com.persist.arquivo.ArquivoModelo;
 import br.com.persist.arquivo.ArquivoTree;
 import br.com.persist.arquivo.ArquivoTreeListener;
 import br.com.persist.arquivo.ArquivoUtil;
+import br.com.persist.assistencia.Constantes;
+import br.com.persist.assistencia.Mensagens;
+import br.com.persist.assistencia.Selecao;
 import br.com.persist.assistencia.Util;
+import br.com.persist.componente.BarraButton;
+import br.com.persist.componente.Nil;
 import br.com.persist.componente.Panel;
+import br.com.persist.componente.ScrollPane;
 import br.com.persist.componente.SplitPane;
+import br.com.persist.componente.TextField;
+import br.com.persist.componente.TextPane;
 import br.com.persist.painel.Fichario;
 import br.com.persist.painel.Separador;
 import br.com.persist.painel.Transferivel;
@@ -125,14 +147,101 @@ class AnotacaoSplit extends SplitPane {
 
 class Aba extends Transferivel {
 	private static final long serialVersionUID = 1L;
+	private final TextPane textArea = new TextPane();
+	private final Toolbar toolbar = new Toolbar();
 	final transient Arquivo arquivo;
 
 	public Aba(Arquivo arquivo) {
 		this.arquivo = Objects.requireNonNull(arquivo);
+		toolbar.ini();
+		montarLayout();
+		abrir();
 	}
 
-	public Arquivo getArquivo() {
-		return arquivo;
+	private void montarLayout() {
+		add(BorderLayout.NORTH, toolbar);
+		add(BorderLayout.CENTER, new ScrollPane(textArea));
+	}
+
+	private void abrir() {
+		textArea.limpar();
+		if (arquivo.getFile().exists()) {
+			try (BufferedReader br = new BufferedReader(
+					new InputStreamReader(new FileInputStream(arquivo.getFile()), StandardCharsets.UTF_8))) {
+				String linha = br.readLine();
+				while (linha != null) {
+					textArea.append(linha + Constantes.QL);
+					linha = br.readLine();
+				}
+			} catch (Exception ex) {
+				Util.stackTraceAndMessage("Aba", ex, Aba.this);
+			}
+		}
+	}
+
+	private class Toolbar extends BarraButton implements ActionListener {
+		private static final long serialVersionUID = 1L;
+		private final TextField txtPesquisa = new TextField(35);
+		private transient Selecao selecao;
+
+		public void ini() {
+			super.ini(new Nil(), BAIXAR, LIMPAR, SALVAR, COPIAR, COLAR);
+			txtPesquisa.setToolTipText(Mensagens.getString("label.pesquisar"));
+			txtPesquisa.addActionListener(this);
+			add(txtPesquisa);
+			add(label);
+		}
+
+		@Override
+		protected void baixar() {
+			abrir();
+			selecao = null;
+			label.limpar();
+		}
+
+		@Override
+		protected void limpar() {
+			textArea.limpar();
+		}
+
+		@Override
+		protected void copiar() {
+			String string = Util.getString(textArea);
+			Util.setContentTransfered(string);
+			copiarMensagem(string);
+			textArea.requestFocus();
+		}
+
+		@Override
+		protected void colar(boolean numeros, boolean letras) {
+			Util.getContentTransfered(textArea, numeros, letras);
+		}
+
+		@Override
+		protected void salvar() {
+			if (Util.confirmaSalvar(Aba.this, Constantes.TRES)) {
+				salvarArquivo(arquivo.getFile());
+			}
+		}
+
+		private void salvarArquivo(File file) {
+			try (PrintWriter pw = new PrintWriter(file, StandardCharsets.UTF_8.name())) {
+				pw.print(textArea.getText());
+				salvoMensagem();
+			} catch (Exception ex) {
+				Util.stackTraceAndMessage("Aba", ex, Aba.this);
+			}
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (!Util.estaVazio(txtPesquisa.getText())) {
+				selecao = Util.getSelecao(textArea, selecao, txtPesquisa.getText());
+				selecao.selecionar(label);
+			} else {
+				label.limpar();
+			}
+		}
 	}
 }
 
