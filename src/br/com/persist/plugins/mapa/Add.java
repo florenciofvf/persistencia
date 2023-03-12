1730a8b8d3c00d3704c943a65765ab15a54809f4
Florencio Vieira Filho <florenciovieira@gmail.com>
2023-03-12 09:23:49 -0300

    Atualização

---
+++ src/br/com/persist/plugins/mapa/Add.java
@@ -0,0 +1,49 @@
+package br.com.persist.plugins.mapa;
+
+import br.com.persist.assistencia.Util;
+
+public class Add {
+	private final String nome;
+
+	public Add(String nome) {
+		if (Util.estaVazio(nome)) {
+			throw new IllegalArgumentException("Nome do objeto vazio.");
+		}
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String toString() {
+		return nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Add other = (Add) obj;
+		if (nome == null) {
+			if (other.nome != null) {
+				return false;
+			}
+		} else if (!nome.equals(other.nome)) {
+			return false;
+		}
+		return true;
+	}
+}
---
+++ src/br/com/persist/plugins/mapa/Ref.java
@@ -0,0 +1,49 @@
+package br.com.persist.plugins.mapa;
+
+import br.com.persist.assistencia.Util;
+
+public class Ref {
+	private final String nome;
+
+	public Ref(String nome) {
+		if (Util.estaVazio(nome)) {
+			throw new IllegalArgumentException("Nome da referencia vazia.");
+		}
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String toString() {
+		return nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ref other = (Ref) obj;
+		if (nome == null) {
+			if (other.nome != null) {
+				return false;
+			}
+		} else if (!nome.equals(other.nome)) {
+			return false;
+		}
+		return true;
+	}
+}
