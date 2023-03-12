332532bbbc778f0fa31a44e88372183b6803244a
Florencio Vieira Filho <florenciovieira@gmail.com>
2023-03-12 10:21:05 -0300

    Atualização

---
+++ src/br/com/persist/plugins/mapa/MapaHandler.java
@@ -0,0 +1,57 @@
+package br.com.persist.plugins.mapa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+
+import br.com.persist.marca.XMLHandler;
+
+public class MapaHandler extends XMLHandler {
+	Set<Objeto> objetos = new HashSet<>();
+	Objeto selecionado;
+	Objeto raiz;
+
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		if (raiz == null) {
+			raiz = new Objeto(qName);
+			raiz.lerAtributos(attributes);
+		} else if ("add".equals(qName)) {
+			if (selecionado == null) {
+				throw new IllegalStateException("add deve possuir um parent.");
+			}
+			selecionado.adicionar(criarAdd(attributes));
+		} else if ("ref".equals(qName)) {
+			if (selecionado == null) {
+				throw new IllegalStateException("ref deve possuir um parent.");
+			}
+			selecionado.adicionar(criarRef(attributes));
+		} else {
+			selecionado = new Objeto(qName);
+			selecionado.lerAtributos(attributes);
+		}
+	}
+
+	private Add criarAdd(Attributes attributes) {
+		return new Add(attributes.getValue("obj"));
+	}
+
+	private Ref criarRef(Attributes attributes) {
+		return new Ref(attributes.getValue("obj"));
+	}
+
+	public Objeto getRaiz() {
+		return raiz;
+	}
+
+	public Objeto getObjeto(String nome) {
+		for (Objeto objeto : objetos) {
+			if (objeto.nome.equalsIgnoreCase(nome)) {
+				return objeto;
+			}
+		}
+		return null;
+	}
+}
