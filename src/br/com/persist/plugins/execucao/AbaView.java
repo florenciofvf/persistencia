da02ff566db9dcae4a8003f85e0c1dad4ceed0a1
Florencio Vieira Filho <florenciovieira@gmail.com>
2023-04-09 21:33:29 -0300

    Atualização

---
+++ src/br/com/persist/plugins/execucao/AbaView.java
@@ -0,0 +1,116 @@
+package br.com.persist.plugins.execucao;
+
+import static br.com.persist.componente.BarraButtonEnum.BAIXAR;
+
+import java.awt.BorderLayout;
+import java.io.File;
+
+import javax.swing.Icon;
+import javax.swing.SwingUtilities;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+
+import br.com.persist.assistencia.Util;
+import br.com.persist.componente.Action;
+import br.com.persist.componente.BarraButton;
+import br.com.persist.componente.Nil;
+import br.com.persist.componente.Panel;
+import br.com.persist.marca.XML;
+import br.com.persist.marca.XMLHandler;
+
+public class AbaView extends Panel {
+	private static final long serialVersionUID = 1L;
+	private ContainerTree tree = new ContainerTree();
+	private final Toolbar toolbar = new Toolbar();
+	private PanelLog log = new PanelLog();
+	private final File file;
+
+	public AbaView(File file) {
+		this.file = file;
+		montarLayout();
+	}
+
+	private void montarLayout() {
+		add(BorderLayout.NORTH, toolbar);
+		add(BorderLayout.CENTER, tree);
+		add(BorderLayout.SOUTH, log);
+	}
+
+	public void carregar(File file) {
+		try {
+			Handler handler = new Handler();
+			if (file.exists() && file.canRead()) {
+				XML.processar(file, handler);
+			}
+			tree.setModel(new ContainerModelo(handler.getRaiz()));
+		} catch (Exception ex) {
+			Util.stackTraceAndMessage(ExecucaoConstantes.PAINEL_EXECUCAO, ex, AbaView.this);
+		}
+		SwingUtilities.updateComponentTreeUI(this);
+	}
+
+	private class Toolbar extends BarraButton {
+		private static final long serialVersionUID = 1L;
+
+		private Toolbar() {
+			super.ini(new Nil(), BAIXAR);
+		}
+
+		@Override
+		protected void baixar() {
+			carregar(file);
+		}
+	}
+
+	static Action actionMenu(String chave, Icon icon) {
+		return Action.acaoMenu(ExecucaoMensagens.getString(chave), icon);
+	}
+
+	static Action actionMenu(String chave) {
+		return actionMenu(chave, null);
+	}
+}
+
+class Handler extends XMLHandler {
+	private Container selecionado;
+	private Container raiz;
+
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		if (raiz == null) {
+			raiz = new Container();
+			raiz.lerAtributos(attributes);
+			selecionado = raiz;
+		} else {
+			Container container = new Container();
+			container.lerAtributos(attributes);
+			selecionado.adicionar(container);
+			selecionado = container;
+		}
+	}
+
+	@Override
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		selecionado = selecionado.getPai();
+	}
+
+	public Container getRaiz() {
+		return raiz;
+	}
+}
+
+class PanelLog extends Panel {
+	private static final long serialVersionUID = 1L;
+	private TextArea textArea = new TextArea();
+
+	PanelLog() {
+		add(BorderLayout.CENTER, textArea);
+	}
+
+	void processar(Container container) {
+		if (container == null) {
+			return;
+		}
+	}
+}
---
+++ src/br/com/persist/plugins/execucao/ContainerTree.java
@@ -0,0 +1,149 @@
+package br.com.persist.plugins.execucao;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.tree.TreePath;
+
+import br.com.persist.assistencia.Constantes;
+import br.com.persist.assistencia.Icones;
+import br.com.persist.componente.Action;
+import br.com.persist.componente.Popup;
+import br.com.persist.componente.Tree;
+
+public class ContainerTree extends Tree {
+	private static final long serialVersionUID = 1L;
+	private final transient List<ContainerTreeListener> ouvintes;
+	private ContainerPopup popup = new ContainerPopup();
+
+	public ContainerTree(ContainerModelo modelo) {
+		super(modelo);
+		addMouseListener(mouseListenerInner);
+		addKeyListener(keyListenerInner);
+		ouvintes = new ArrayList<>();
+	}
+
+	public ContainerTree() {
+		this(new ContainerModelo(new Container()));
+	}
+
+	public void adicionarOuvinte(ContainerTreeListener listener) {
+		if (listener != null) {
+			ouvintes.add(listener);
+		}
+	}
+
+	public ContainerModelo getModelo() {
+		return (ContainerModelo) getModel();
+	}
+
+	public Container getRaiz() {
+		return (Container) getModelo().getRoot();
+	}
+
+	public Container getObjetoSelecionado() {
+		TreePath path = getSelectionPath();
+		if (path == null) {
+			return null;
+		}
+		if (path.getLastPathComponent() instanceof Container) {
+			return (Container) path.getLastPathComponent();
+		}
+		return null;
+	}
+
+	private transient KeyAdapter keyListenerInner = new KeyAdapter() {
+		@Override
+		public void keyReleased(KeyEvent e) {
+			if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+				mouseListenerInner
+						.mouseClicked(new MouseEvent(ContainerTree.this, 0, 0, 0, 0, 0, Constantes.DOIS, false));
+			}
+		}
+	};
+
+	private transient MouseListener mouseListenerInner = new MouseAdapter() {
+		@Override
+		public void mousePressed(MouseEvent e) {
+			processar(e);
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			processar(e);
+		}
+
+		private void processar(MouseEvent e) {
+			popupTrigger = false;
+			checkPopupTrigger(e);
+			if (!e.isPopupTrigger() || getObjetoSelecionado() == null) {
+				return;
+			}
+			TreePath clicado = getClosestPathForLocation(e.getX(), e.getY());
+			TreePath selecionado = getSelectionPath();
+			popupTrigger = true;
+			if (!validos(clicado, selecionado)) {
+				setSelectionPath(null);
+				return;
+			}
+			if (!localValido(clicado, e)) {
+				setSelectionPath(null);
+				return;
+			}
+			if (clicado.equals(selecionado)) {
+				if (selecionado.getLastPathComponent() instanceof Container) {
+					Container container = (Container) selecionado.getLastPathComponent();
+					popup.preShow(container);
+					popup.show(ContainerTree.this, e.getX(), e.getY());
+				} else {
+					setSelectionPath(null);
+				}
+			} else {
+				setSelectionPath(null);
+			}
+		}
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			if (popupTrigger) {
+				return;
+			}
+			TreePath clicado = getClosestPathForLocation(e.getX(), e.getY());
+			TreePath selecionado = getSelectionPath();
+			if (!validos(clicado, selecionado)) {
+				setSelectionPath(null);
+				return;
+			}
+			if (!localValido(clicado, e)) {
+				setSelectionPath(null);
+				return;
+			}
+			if (e.getClickCount() >= Constantes.DOIS) {
+				Container arquivo = getObjetoSelecionado();
+				if (arquivo == null) {
+					return;
+				}
+				ouvintes.forEach(o -> o.executar(ContainerTree.this));
+			}
+		}
+	};
+
+	private class ContainerPopup extends Popup {
+		private static final long serialVersionUID = 1L;
+		private Action executarAcao = Action.actionMenu("label.executar", Icones.EXECUTAR);
+
+		private ContainerPopup() {
+			addMenuItem(executarAcao);
+			executarAcao.setActionListener(e -> ouvintes.forEach(o -> o.executar(ContainerTree.this)));
+		}
+
+		private void preShow(Container container) {
+			executarAcao.setEnabled(container != null);
+		}
+	}
+}
