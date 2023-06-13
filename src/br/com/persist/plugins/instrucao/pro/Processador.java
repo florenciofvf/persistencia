1c081a7f833a25db03c603b9812603dbf93296e6
Florencio Vieira Filho <florenciovieira@gmail.com>
2023-06-13 13:55:52 -0300

    Atualização

--- src/br/com/persist/plugins/instrucao/pro/Processador.java
+++ src/br/com/persist/plugins/instrucao/pro/Processador.java
@@ -15,6 +15,7 @@ import br.com.persist.plugins.instrucao.inst.Ifeq;
 import br.com.persist.plugins.instrucao.inst.Igual;
 import br.com.persist.plugins.instrucao.inst.Invoke;
 import br.com.persist.plugins.instrucao.inst.Load;
+import br.com.persist.plugins.instrucao.inst.LoadVar;
 import br.com.persist.plugins.instrucao.inst.Maior;
 import br.com.persist.plugins.instrucao.inst.MaiorI;
 import br.com.persist.plugins.instrucao.inst.Menor;
@@ -79,6 +80,7 @@ public class Processador {
 		Instrucoes.add(new Igual(null));
 		Instrucoes.add(new Invoke(null));
 		Instrucoes.add(new Load(null));
+		Instrucoes.add(new LoadVar(null));
 		Instrucoes.add(new Maior(null));
 		Instrucoes.add(new MaiorI(null));
 		Instrucoes.add(new Menor(null));
