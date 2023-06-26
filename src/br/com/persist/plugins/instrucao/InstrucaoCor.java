33be55a90f352bc6ac4e4da31b8f6bee90c56e7d
Florencio Vieira Filho <florenciovieira@gmail.com>
2023-06-25 22:25:56 -0300

    Atualização

--- src/br/com/persist/plugins/instrucao/InstrucaoCor.java
+++ src/br/com/persist/plugins/instrucao/InstrucaoCor.java
@@ -11,8 +11,8 @@ import javax.swing.text.StyledDocument;
 import br.com.persist.plugins.instrucao.cmpl.Atom;
 
 public class InstrucaoCor {
+	private final MutableAttributeSet attLightGray;
 	private final MutableAttributeSet attMagenta;
-	private final MutableAttributeSet attGreen2;
 	private final MutableAttributeSet attBlack;
 	private final MutableAttributeSet attGreen;
 	private final MutableAttributeSet attBlue;
@@ -21,24 +21,24 @@ public class InstrucaoCor {
 	private final MutableAttributeSet attRed;
 
 	public InstrucaoCor() {
+		attLightGray = new SimpleAttributeSet();
 		attMagenta = new SimpleAttributeSet();
-		attGreen2 = new SimpleAttributeSet();
 		attBlack = new SimpleAttributeSet();
 		attGreen = new SimpleAttributeSet();
 		attBlue = new SimpleAttributeSet();
 		attPink = new SimpleAttributeSet();
 		attRed2 = new SimpleAttributeSet();
 		attRed = new SimpleAttributeSet();
+		StyleConstants.setForeground(attLightGray, Color.LIGHT_GRAY);
 		StyleConstants.setForeground(attGreen, new Color(0, 125, 0));
 		StyleConstants.setForeground(attRed2, new Color(125, 0, 0));
 		StyleConstants.setForeground(attMagenta, Color.MAGENTA);
-		StyleConstants.setForeground(attGreen2, Color.GREEN);
 		StyleConstants.setForeground(attBlack, Color.BLACK);
 		StyleConstants.setForeground(attBlue, Color.BLUE);
 		StyleConstants.setForeground(attPink, Color.PINK);
 		StyleConstants.setForeground(attRed, Color.RED);
+		StyleConstants.setBold(attLightGray, true);
 		StyleConstants.setBold(attMagenta, true);
-		StyleConstants.setBold(attGreen2, true);
 		StyleConstants.setBold(attBlack, true);
 		StyleConstants.setBold(attGreen, true);
 		StyleConstants.setBold(attBlue, true);
@@ -56,7 +56,7 @@ public class InstrucaoCor {
 			} else if (atom.isFuncaoInfixa()) {
 				set(doc, atom, attRed);
 			} else if (atom.isComentario()) {
-				set(doc, atom, attGreen2);
+				set(doc, atom, attLightGray);
 			} else if (atom.isVariavel()) {
 				set(doc, atom, attMagenta);
 			} else if (atom.isVirgula()) {
