2e59e7246da18851a949d87d602eb7d262a56317
Florencio Vieira Filho <florenciovieira@gmail.com>
2023-10-26 08:58:56 -0300

    Atualização

--- src/br/com/persist/assistencia/Constantes.java
+++ src/br/com/persist/assistencia/Constantes.java
@@ -28,7 +28,7 @@ public class Constantes {
 	public static final String LABEL_SEM_ASPAS = "label.sem_aspas";
 	public static final String LABEL_APARENCIA = "label.aparencia";
 	public static final String GC_PREFERENCIAL = "GC_PREFERENCIAL";
-	public static final Dimension SIZE2 = new Dimension(900, 620);
+	public static final Dimension SIZE2 = new Dimension(900, 700);
 	public static final Dimension SIZE3 = new Dimension(400, 400);
 	public static final Dimension SIZE = new Dimension(1000, 700);
 	public static final Stroke STROKE_PADRAO = new BasicStroke(2);
