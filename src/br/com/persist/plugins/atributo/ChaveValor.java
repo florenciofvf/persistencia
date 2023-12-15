f21f20ed28ec446794b1495ba25f18707a3c4079
Florencio Vieira Filho <florenciovieira@gmail.com>
2023-12-15 09:31:05 -0300

    Atualização

---
+++ src/br/com/persist/plugins/atributo/ChaveValor.java
@@ -0,0 +1,46 @@
+package br.com.persist.plugins.atributo;
+
+import java.util.Objects;
+
+public class ChaveValor {
+	private final String chave;
+	private final Object valor;
+
+	public ChaveValor(String chave, Object valor) {
+		this.chave = chave;
+		this.valor = valor;
+	}
+
+	public String getChave() {
+		return chave;
+	}
+
+	public Object getValor() {
+		return valor;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(chave);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		ChaveValor other = (ChaveValor) obj;
+		return Objects.equals(chave, other.chave);
+	}
+
+	@Override
+	public String toString() {
+		return chave;
+	}
+}
---
+++ src/br/com/persist/plugins/atributo/Mapa.java
@@ -0,0 +1,65 @@
+package br.com.persist.plugins.atributo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.persist.assistencia.Util;
+
+public class Mapa {
+	private final List<ChaveValor> lista;
+
+	public Mapa() {
+		lista = new ArrayList<>();
+	}
+
+	public void put(String chave, Object valor) {
+		if (Util.isEmpty(chave) || valor == null) {
+			return;
+		}
+		ChaveValor cv = new ChaveValor(chave, valor);
+		if (lista.contains(cv) || !valorValido(valor)) {
+			return;
+		}
+		lista.add(cv);
+	}
+
+	public Object get(String chave) {
+		if (Util.isEmpty(chave)) {
+			return null;
+		}
+		for (ChaveValor cv : lista) {
+			if (cv.getChave().equals(chave)) {
+				return cv.getValor();
+			}
+		}
+		return null;
+	}
+
+	public int getSize() {
+		return lista.size();
+	}
+
+	public void clear() {
+		lista.clear();
+	}
+
+	public List<String> getChaves() {
+		List<String> resp = new ArrayList<>();
+		for (ChaveValor cv : lista) {
+			resp.add(cv.getChave());
+		}
+		return resp;
+	}
+
+	public List<Object> getValores() {
+		List<Object> resp = new ArrayList<>();
+		for (ChaveValor cv : lista) {
+			resp.add(cv.getValor());
+		}
+		return resp;
+	}
+
+	private boolean valorValido(Object valor) {
+		return (valor instanceof String) || (valor instanceof Mapa);
+	}
+}
