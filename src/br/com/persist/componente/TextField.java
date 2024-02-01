5de5a1064bfe8f62ce586424b9836feec5375a3a
Florencio Vieira Filho <florenciovieira@gmail.com>
2024-02-01 11:10:22 -0300

    Atualização

--- src/br/com/persist/componente/TextField.java
+++ src/br/com/persist/componente/TextField.java
@@ -1,5 +1,8 @@
 package br.com.persist.componente;
 
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
 import javax.swing.JTextField;
 
 import br.com.persist.assistencia.Constantes;
@@ -20,5 +23,59 @@ public class TextField extends JTextField {
 
 	public void limpar() {
 		setText(Constantes.VAZIO);
+	}
+
+	public void ignorarMinusculo() {
+		addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				processar(e);
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				processar(e);
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				processar(e);
+			}
+
+			public void processar(KeyEvent e) {
+				if (lower(e.getKeyChar())) {
+					e.consume();
+				}
+			}
+
+			private boolean lower(char c) {
+				return c >= 'a' && c <= 'z';
+			}
+		});
+	}
+
+	public void ignorarEspaco() {
+		addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				processar(e);
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				processar(e);
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				processar(e);
+			}
+
+			public void processar(KeyEvent e) {
+				if (e.getKeyChar() == ' ') {
+					e.consume();
+				}
+			}
+		});
 	}
 }
