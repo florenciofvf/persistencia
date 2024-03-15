c553d4cb83e4c04f5ddfc07ab57c5f96d8e8d37a
Florencio Vieira Filho <florenciovieira@gmail.com>
2024-03-15 13:47:55 -0300

    Atualização

--- src/br/com/persist/plugins/persistencia/tabela/TabelaPersistencia.java
+++ src/br/com/persist/plugins/persistencia/tabela/TabelaPersistencia.java
@@ -264,7 +264,7 @@ public class TabelaPersistencia extends JTable {
 		TableColumn tableColumn = getTableColumn(coluna);
 		CabecalhoColuna cabecalho = (CabecalhoColuna) tableColumn.getHeaderRenderer();
 		if (cabecalho != null) {
-			cabecalho.setBackground(Color.RED);
+			cabecalho.setBackground(Color.BLUE);
 			SwingUtilities.updateComponentTreeUI(this);
 			tornarVisivel(0, coluna);
 		}
