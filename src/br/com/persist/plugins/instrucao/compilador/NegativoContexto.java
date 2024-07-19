ddfea73d922f68fe272e1d7d744d4af1065ffba6
Florencio Vieira Filho <florenciovieira@gmail.com>
2024-07-19 13:42:35 -0300

    Atualização

--- src/br/com/persist/plugins/instrucao/compilador/NegativoContexto.java
+++ src/br/com/persist/plugins/instrucao/compilador/NegativoContexto.java
@@ -1,5 +1,12 @@
 package br.com.persist.plugins.instrucao.compilador;
 
+import java.io.PrintWriter;
+
 public class NegativoContexto extends Container {
 	public static final String NEG = "neg";
+
+	@Override
+	public void salvar(PrintWriter pw) {
+		print(pw, NEG);
+	}
 }
