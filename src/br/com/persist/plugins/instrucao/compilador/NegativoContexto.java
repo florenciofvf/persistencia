a8e98c4cfc2072112b168e0e32fca8844e12ee2f
Florencio Vieira Filho <florenciovieira@gmail.com>
2024-07-21 07:18:43 -0300

    Atualização

--- src/br/com/persist/plugins/instrucao/compilador/NegativoContexto.java
+++ src/br/com/persist/plugins/instrucao/compilador/NegativoContexto.java
@@ -1,9 +1,16 @@
 package br.com.persist.plugins.instrucao.compilador;
 
 import java.io.PrintWriter;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class NegativoContexto extends Container {
 	public static final String NEG = "neg";
+
+	@Override
+	public void indexar(AtomicInteger atomic) {
+		super.indexar(atomic);
+		indice = atomic.getAndIncrement();
+	}
 
 	@Override
 	public void salvar(PrintWriter pw) {
