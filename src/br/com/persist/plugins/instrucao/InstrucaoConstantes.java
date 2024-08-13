e57e5e0d882eb1850a52435a57a8e294025bbe3a
Florencio Vieira Filho <florenciovieira@gmail.com>
2024-08-13 11:58:35 -0300

    Atualização

--- src/br/com/persist/plugins/instrucao/InstrucaoConstantes.java
+++ src/br/com/persist/plugins/instrucao/InstrucaoConstantes.java
@@ -14,8 +14,8 @@ public class InstrucaoConstantes {
 	public static final String ESPACO = " ";
 
 	public static final String PREFIXO_FUNCAO_NATIVA = "funcao_nativa ";
-	public static final String PREFIXO_PARAMETRO = "parametro ";
 	public static final String PREFIXO_CONSTANTE = "constante ";
+	public static final String PREFIXO_PARAMETRO = "param ";
 	public static final String PREFIXO_FUNCAO = "funcao ";
 
 	private InstrucaoConstantes() {
