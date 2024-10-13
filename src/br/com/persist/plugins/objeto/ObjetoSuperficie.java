a55bd8ad68f067ad90fcb46c06563a5497d7ed93
Florencio Vieira Filho <florenciovieira@gmail.com>
2024-10-13 17:17:51 -0300

    Atualização

--- src/br/com/persist/plugins/objeto/ObjetoSuperficie.java
+++ src/br/com/persist/plugins/objeto/ObjetoSuperficie.java
@@ -825,7 +825,7 @@ public class ObjetoSuperficie extends Desktop implements ObjetoListener, Relacao
 		}
 		InternalFormulario interno = ObjetoSuperficieUtil.getInternalFormulario(ObjetoSuperficie.this, objeto);
 		if (interno != null) {
-			aproximarObjetoFormularioImpl(false, false, interno);
+			menuAjuste.aproximarEmpilharUsarForms();
 			larguras.configurar(DesktopLargura.TOTAL_A_DIREITA, interno);
 		}
 	}
