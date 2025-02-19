5bfae0946f12cf7fc9d00974b872da7cefc667af
Florencio Vieira Filho <florenciovieira@gmail.com>
2025-02-19 10:31:31 -0300

    Atualização

--- src/br/com/persist/plugins/instrucao/biblionativo/IClass.java
+++ src/br/com/persist/plugins/instrucao/biblionativo/IClass.java
@@ -4,8 +4,16 @@ public class IClass {
 	private IClass() {
 	}
 
-	@Biblio
+	@Biblio(1)
 	public static Object get(Object absoluto) throws ClassNotFoundException {
 		return Class.forName((String) absoluto);
+	}
+
+	@Biblio(2)
+	public static Object getType(Object objeto) {
+		if (objeto == null) {
+			return "null";
+		}
+		return objeto.getClass().getName();
 	}
 }
