6109dc73835b99ae632e1eb2cd03c684ce830d50
Florencio Vieira Filho <florenciovieira@gmail.com>
2025-02-27 13:32:09 -0300

    Atualização

--- src/br/com/persist/plugins/instrucao/biblionativo/IParse.java
+++ src/br/com/persist/plugins/instrucao/biblionativo/IParse.java
@@ -2,8 +2,12 @@ package br.com.persist.plugins.instrucao.biblionativo;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class IParse {
+	private static final Conversor CONVERSOR = new Conversor();
+
 	private IParse() {
 	}
 
@@ -15,5 +19,26 @@ public class IParse {
 	@Biblio(1)
 	public static BigDecimal bigDecimal(Object object) {
 		return new BigDecimal(object.toString());
+	}
+
+	@Biblio(2)
+	public static Object dateUS(Object stringUS) {
+		return CONVERSOR.dateUS(stringUS);
+	}
+
+	static class Conversor {
+		private DateFormat dateFormatUS = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+		Object dateUS(Object object) {
+			if (object == null) {
+				return "";
+			}
+			String string = object.toString();
+			try {
+				return dateFormatUS.parse(string);
+			} catch (Exception ex) {
+				return "";
+			}
+		}
 	}
 }
