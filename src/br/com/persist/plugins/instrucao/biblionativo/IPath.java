3d38fed28cf12a1cadf9632e4119d5c226d39032
Florencio Vieira Filho <florenciovieira@gmail.com>
2025-03-29 09:34:35 -0300

    Atualização

---
+++ src/br/com/persist/plugins/instrucao/biblionativo/IPath.java
@@ -0,0 +1,33 @@
+package br.com.persist.plugins.instrucao.biblionativo;
+
+import java.io.File;
+
+import br.com.persist.assistencia.Util;
+
+public class IPath {
+	private IPath() {
+	}
+
+	@Biblio(0)
+	public static String get(Object object) {
+		if (object == null) {
+			return "";
+		}
+		String string = object.toString();
+		if (File.pathSeparatorChar == '/') {
+			string = Util.replaceAll(string, "\\", "/");
+		} else if (File.pathSeparatorChar == '\\') {
+			string = Util.replaceAll(string, "/", "\\");
+		}
+		return string;
+	}
+
+	@Biblio(1)
+	public static String absolute(Object object) {
+		if (object == null) {
+			return "";
+		}
+		String string = object.toString();
+		return new File(string).getAbsolutePath();
+	}
+}
