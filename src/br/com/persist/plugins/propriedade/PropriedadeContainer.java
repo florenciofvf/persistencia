c88772929462ee014438daf80cd012ec83185b8c
Florencio Vieira Filho <florenciovieira@gmail.com>
2025-04-09 08:44:42 -0300

    Atualização

--- src/br/com/persist/plugins/propriedade/PropriedadeContainer.java
+++ src/br/com/persist/plugins/propriedade/PropriedadeContainer.java
@@ -19,8 +19,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.io.File;
-import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
 
 import javax.swing.Icon;
 import javax.swing.JComboBox;
@@ -352,8 +350,8 @@ public class PropriedadeContainer extends AbstratoContainer {
 			if (aba == null || !aba.isValido()) {
 				return;
 			}
-			try (PrintWriter pw = new PrintWriter(aba.file, StandardCharsets.UTF_8.name())) {
-				pw.print(aba.getText());
+			try {
+				ArquivoUtil.salvar(aba.editor, aba.file);
 				salvoMensagem();
 			} catch (Exception ex) {
 				Util.stackTraceAndMessage(PropriedadeConstantes.PAINEL_PROPRIEDADE, ex, PropriedadeContainer.this);
