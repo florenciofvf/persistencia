902858531fe126b44b9aa8d1a79f3bf96d5827c9
Florencio Vieira Filho <florenciovieira@gmail.com>
2025-06-19 11:32:51 -0300

    Atualização

---
+++ src/br/com/persist/plugins/objeto/config/ItemCor.java
@@ -0,0 +1,28 @@
+package br.com.persist.plugins.objeto.config;
+
+import java.awt.Color;
+import java.util.Objects;
+
+public class ItemCor {
+	final int linha;
+	final int coluna;
+	final Color color;
+
+	public ItemCor(int linha, int coluna, Color color) {
+		this.color = Objects.requireNonNull(color);
+		this.coluna = coluna;
+		this.linha = linha;
+	}
+
+	public int getLinha() {
+		return linha;
+	}
+
+	public int getColuna() {
+		return coluna;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+}
