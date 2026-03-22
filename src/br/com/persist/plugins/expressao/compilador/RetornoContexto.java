197d03bee44802435a965b9f88ce2b3923d71418
Florencio Vieira Filho <florenciovieira@gmail.com>
2026-03-22 20:18:20 -0300

    Atualização

--- src/br/com/persist/plugins/expressao/compilador/RetornoContexto.java
+++ src/br/com/persist/plugins/expressao/compilador/RetornoContexto.java
@@ -1,18 +1,29 @@
 package br.com.persist.plugins.expressao.compilador;
 
 public class RetornoContexto extends Contexto {
+	private TokenExec[] execs = { new PontoEVirgulaOuAbreParentese(), new PontoEVirgula() };
+
 	@Context("retorno_da_funcao")
 	@Doc({ "return;", "return expressao;" })
 	@Override
 	public void processar(Compilador compilador, Token token) {
-		if (token.isPontoEVirgula()) {
-			compilador.setSelecionado(parent);
-		} else if (token.isAbreParentese()) {
-			ExpressaoContexto expressao = new ExpressaoContexto();
-			compilador.setSelecionado(expressao);
-			add(expressao);
-		} else {
-			compilador.invalidar(token);
+		execs[indiceEstado].processar(compilador, token);
+	}
+
+	class PontoEVirgulaOuAbreParentese implements TokenExec {
+		@Override
+		public void processar(Compilador compilador, Token token) {
+			if (token.isPontoEVirgula()) {
+				compilador.setSelecionado(parent);
+				indiceEstado++;
+			} else if (token.isAbreParentese()) {
+				ExpressaoContexto expressao = new ExpressaoContexto();
+				compilador.setSelecionado(expressao);
+				add(expressao);
+				indiceEstado++;
+			} else {
+				compilador.invalidar(token);
+			}
 		}
 	}
 }
