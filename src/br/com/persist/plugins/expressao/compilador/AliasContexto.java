fc5c5317275a1dfee8bfcfee16e49efd279fa89e
Florencio Vieira Filho <florenciovieira@gmail.com>
2026-03-22 17:58:23 -0300

    Atualização

--- src/br/com/persist/plugins/expressao/compilador/AliasContexto.java
+++ src/br/com/persist/plugins/expressao/compilador/AliasContexto.java
@@ -1,10 +1,35 @@
 package br.com.persist.plugins.expressao.compilador;
 
 public class AliasContexto extends Contexto {
-	/*
-	 * alias pacote nome;
-	 */
+	private Token pacote;
+	private Token chave;
+
+	@Context("alias_para_biblioteca")
+	@Doc({ "alias chaveN chave;" })
 	@Override
 	public void processar(Compilador compilador, Token token) {
+		if (token.isChaveN()) {
+			if (pacote != null) {
+				compilador.invalidar(token, "pacote_ja_definido");
+			} else {
+				pacote = token;
+			}
+		} else if (token.isChave()) {
+			if (chave != null) {
+				compilador.invalidar(token, "chave_ja_definida");
+			} else {
+				chave = token;
+			}
+		} else if (token.isPontoEVirgula()) {
+			if (pacote == null) {
+				compilador.invalidar(token, "pacote_nao_definido");
+			} else if (chave == null) {
+				compilador.invalidar(token, "chave_nao_definida");
+			} else {
+				compilador.setSelecionado(parent);
+			}
+		} else {
+			compilador.invalidar(token);
+		}
 	}
 }
