cf301dfc0932e5003066ba1ee022d66a85ad677a
Florencio Vieira Filho <florenciovieira@gmail.com>
2026-03-22 22:08:41 -0300

    Atualização

---
+++ src/br/com/persist/plugins/expressao/compilador/FuncaoNativaContexto.java
@@ -0,0 +1,44 @@
+package br.com.persist.plugins.expressao.compilador;
+
+import br.com.persist.plugins.expressao.ExpressaoConstantes;
+
+public class FuncaoNativaContexto extends Contexto {
+	private TokenExec[] execs = { new IniParametros(), new TipoRetornoOuPontoEVirgula(), new PontoEVirgula() };
+	protected boolean retornoVoid;
+
+	@Context("funcao_nativa")
+	@Doc({ "funcao_nativa parametros;", "funcao_nativa parametros void;" })
+	@Override
+	public void processar(Compilador compilador, Token token) {
+		execs[indiceEstado].processar(compilador, token);
+	}
+
+	class IniParametros implements TokenExec {
+		public void processar(Compilador compilador, Token token) {
+			if (token.isAbreParentese()) {
+				ParametrosContexto parametros = new ParametrosContexto();
+				compilador.setSelecionado(parametros);
+				add(parametros);
+				indiceEstado++;
+			} else {
+				compilador.invalidar(token);
+			}
+		}
+	}
+
+	class TipoRetornoOuPontoEVirgula implements TokenExec {
+		@Override
+		public void processar(Compilador compilador, Token token) {
+			if (token.isPontoEVirgula()) {
+				execs = new TokenExec[] { new IniParametros(), new TipoRetornoOuPontoEVirgula() };
+				compilador.setSelecionado(parent);
+				indiceEstado++;
+			} else if (ExpressaoConstantes.VOID.equals(token.getString())) {
+				retornoVoid = true;
+				indiceEstado++;
+			} else {
+				compilador.invalidar(token);
+			}
+		}
+	}
+}
