d8cad699df6e02af7ec68cd384caca86ec7d47f8
Florencio Vieira Filho <florenciovieira@gmail.com>
2026-04-04 19:27:53 -0300

    Atualização

--- src/br/com/persist/plugins/expressao/compl/biblio/ConstanteContexto.java
+++ src/br/com/persist/plugins/expressao/compl/biblio/ConstanteContexto.java
@@ -10,23 +10,41 @@ import br.com.persist.plugins.expressao.compl.Contexto;
 import br.com.persist.plugins.expressao.compl.Doc;
 import br.com.persist.plugins.expressao.compl.Token;
 import br.com.persist.plugins.expressao.compl.TokenExec;
+import br.com.persist.plugins.expressao.compl.instrucoes.ExpressaoContexto;
 
 public class ConstanteContexto extends Contexto {
-	private TokenExec[] execs = { new Chave(), new Atribuicao(), new IniExpressao(), new PontoEVirgula() };
 	public static final String DEF_CONST = "def_const";
+	private TokenExec selecionado = new Id();
+
+	@Override
+	protected void selecionadoVia(Compilador compilador, Contexto contexto) throws ExpressaoException {
+		compilador.selecionarParentDe(this);
+	}
 
 	@Context("declaracao_constante")
 	@Doc("const chave = expressao;")
 	@Override
 	public void processar(Compilador compilador, Token token) throws ExpressaoException {
-		checarIndiceEstado(compilador, execs, token);
-		execs[indiceEstado].processar(compilador, token);
+		selecionado.processar(compilador, token);
+	}
+
+	public class Id implements TokenExec {
+		public void processar(Compilador compilador, Token token) throws ExpressaoException {
+			if (token.isChave()) {
+				ConstanteContexto.this.token = token;
+				selecionado = new Atribuicao();
+			} else {
+				compilador.invalidar(token);
+			}
+		}
 	}
 
 	class Atribuicao implements TokenExec {
 		public void processar(Compilador compilador, Token token) throws ExpressaoException {
 			if (token.isAtribuicao()) {
-				indiceEstado++;
+				ExpressaoContexto expressao = new ExpressaoContexto(";");
+				compilador.selecionar(expressao);
+				adicionar(expressao);
 			} else {
 				compilador.invalidar(token);
 			}
