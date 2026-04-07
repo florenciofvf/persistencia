85129c182a4547e50af63cf0b17bb8ca4f401259
Florencio Vieira Filho <florenciovieira@gmail.com>
2026-04-07 11:29:35 -0300

    Atualização

---
+++ src/br/com/persist/plugins/expressao/condicional/IFTest.java
@@ -0,0 +1,34 @@
+package br.com.persist.plugins.expressao.condicional;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Test;
+
+import br.com.persist.plugins.expressao.ExpressaoException;
+import br.com.persist.plugins.expressao.ExpressaoTest;
+import br.com.persist.plugins.expressao.compilador.Compilacao;
+import br.com.persist.plugins.expressao.processador.Processador;
+
+public class IFTest extends ExpressaoTest {
+
+	@Test
+	public void teste10() throws IOException, ExpressaoException {
+		Compilacao compilacao = new Compilacao();
+		compilacao.compilar(getFile("condicional", "__simples10"));
+
+		Processador processador = new Processador();
+
+		String biblio = "br.com.teste.__simples10";
+
+		List<Object> result;
+
+		result = processador.processar(biblio, "se", bi(3), "IF", "ELSE");
+		assertEquals("[ELSE]", result.toString());
+
+		result = processador.processar(biblio, "teste", bi(3), "A", "B");
+		assertEquals("[0]", result.toString());
+	}
+}
---
+++ src/br/com/persist/plugins/expressao/loop/WhileTest.java
@@ -0,0 +1,31 @@
+package br.com.persist.plugins.expressao.loop;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Test;
+
+import br.com.persist.plugins.expressao.ExpressaoException;
+import br.com.persist.plugins.expressao.ExpressaoTest;
+import br.com.persist.plugins.expressao.compilador.Compilacao;
+import br.com.persist.plugins.expressao.processador.Processador;
+
+public class WhileTest extends ExpressaoTest {
+
+	@Test
+	public void teste11() throws IOException, ExpressaoException {
+		Compilacao compilacao = new Compilacao();
+		compilacao.compilar(getFile("loop", "__simples11"));
+
+		Processador processador = new Processador();
+
+		String biblio = "br.com.teste.__simples11";
+
+		List<Object> result;
+
+		result = processador.processar(biblio, "main");
+		assertEquals("[5050]", result.toString());
+	}
+}
