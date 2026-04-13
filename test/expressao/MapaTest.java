6ad4169f450630bb407e41afedde3de26e75d847
Florencio Vieira Filho <florenciovieira@gmail.com>
2026-04-12 21:07:42 -0300

    Atualização

--- test/expressao/MapaTest.java
+++ test/expressao/MapaTest.java
@@ -1,11 +1,15 @@
 package expressao;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
+import java.util.List;
 
 import org.junit.Test;
 
 import br.com.persist.plugins.expressao.ExpressaoException;
 import br.com.persist.plugins.expressao.compilador.Compilacao;
+import br.com.persist.plugins.expressao.processador.Processador;
 
 public class MapaTest extends ExpressaoTest {
 	private static final String MAPA = "mapa";
@@ -14,5 +18,20 @@ public class MapaTest extends ExpressaoTest {
 	public void teste0() throws IOException, ExpressaoException {
 		Compilacao compilacao = new Compilacao();
 		compilacao.compilar(getFile(MAPA, "__mapa0"));
+	}
+
+	@Test
+	public void teste1() throws IOException, ExpressaoException {
+		Compilacao compilacao = new Compilacao();
+		compilacao.compilar(getFile(MAPA, "__teste1"));
+
+		Processador processador = new Processador();
+
+		String biblio = "br.com.teste.__teste1";
+
+		List<Object> result;
+
+		result = processador.processar(biblio, "main");
+		assertEquals("[NOME=florencio vieira filho]", result.toString());
 	}
 }
