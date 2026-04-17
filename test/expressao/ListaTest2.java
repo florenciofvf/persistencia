911afc6210f6071b6c6ea07774b11ece75c96579
Florencio Vieira Filho <florenciovieira@gmail.com>
2026-04-17 04:44:42 -0300

    Atualização

---
+++ test/expressao/ListaTest2.java
@@ -0,0 +1,46 @@
+package expressao;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Test;
+
+import br.com.persist.plugins.expressao.ExpressaoException;
+import br.com.persist.plugins.expressao.compilador.Compilacao;
+import br.com.persist.plugins.expressao.processador.Processador;
+
+public class ListaTest2 extends ExpressaoTest {
+	private static final String LISTA = "lista";
+
+	@Test
+	public void quicksort1() throws IOException, ExpressaoException {
+		Compilacao compilacao = new Compilacao();
+		compilacao.compilar(getFile(LISTA, "quicksort1"));
+
+		Processador processador = new Processador();
+
+		String biblio = "br.com.teste.quicksort1";
+
+		List<Object> result;
+
+		result = processador.processar(biblio, "testarQS");
+		assertEquals("[[-3, 0, 1, 2, 4, 6, 50]]", result.toString());
+	}
+
+	@Test
+	public void quicksort2() throws IOException, ExpressaoException {
+		Compilacao compilacao = new Compilacao();
+		compilacao.compilar(getFile(LISTA, "quicksort2"));
+
+		Processador processador = new Processador();
+
+		String biblio = "br.com.teste.quicksort2";
+
+		List<Object> result;
+
+		result = processador.processar(biblio, "main");
+		assertEquals("[[-3, 0, 1, 2, 4, 6, 46, 50]]", result.toString());
+	}
+}
