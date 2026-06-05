ccd87fd26f9c5f1cb0f46a7623bece5cfb158ab7
Florencio Vieira Filho <florenciovieira@gmail.com>
2026-06-05 12:00:54 -0300

    Atualização

--- src/br/com/persist/plugins/projeto/ProjetoConfiguracao.java
+++ src/br/com/persist/plugins/projeto/ProjetoConfiguracao.java
@@ -1,9 +1,15 @@
 package br.com.persist.plugins.projeto;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
 import java.awt.Insets;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
+import javax.swing.JColorChooser;
 import javax.swing.JRadioButton;
 import javax.swing.SwingConstants;
 
@@ -12,6 +18,7 @@ import br.com.persist.assistencia.Mensagens;
 import br.com.persist.assistencia.Muro;
 import br.com.persist.componente.CheckBox;
 import br.com.persist.componente.Label;
+import br.com.persist.componente.Panel;
 import br.com.persist.componente.PanelCenter;
 import br.com.persist.formulario.Formulario;
 
@@ -38,6 +45,8 @@ public class ProjetoConfiguracao extends AbstratoConfiguracao {
 		Muro muro = new Muro();
 		Label tituloLocalAbas = criarLabelTituloRotulo("label.local_abas");
 		muro.camada(Muro.panelGridBorderBottom(tituloLocalAbas, panelPosicoes));
+		muro.camada(Muro.panelGridBorderBottom(criarLabelTitulo("label.titulo_painel_elemento_final_rest"),
+				new PainelElementoFinalRest()));
 		muro.camada(Muro.panelGrid(chkExibirArqIgnorados));
 		add(BorderLayout.CENTER, muro);
 	}
@@ -47,8 +56,43 @@ public class ProjetoConfiguracao extends AbstratoConfiguracao {
 				.addActionListener(e -> ProjetoPreferencia.setExibirArqIgnorados(chkExibirArqIgnorados.isSelected()));
 	}
 
+	private class PainelElementoFinalRest extends Panel {
+		private Label labelCorAtual = new Label(ProjetoMensagens.getString("label.cor_atual"), false);
+		private transient MouseInner mouseInner = new MouseInner();
+		private static final long serialVersionUID = 1L;
+
+		private PainelElementoFinalRest() {
+			super(new GridLayout(0, 1));
+			add(labelCorAtual);
+			labelCorAtual.setForeground(ProjetoPreferencia.getCorElementoFinalRest());
+			labelCorAtual.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+			labelCorAtual.setHorizontalAlignment(SwingConstants.CENTER);
+			labelCorAtual.addMouseListener(mouseInner);
+		}
+
+		private class MouseInner extends MouseAdapter {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Label label = (Label) e.getSource();
+				Color color = JColorChooser.showDialog(ProjetoConfiguracao.this, label.getText(),
+						label.getForeground());
+				if (color == null) {
+					return;
+				}
+				label.setForeground(color);
+				ProjetoPreferencia.setCorElementoFinalRest(color);
+			}
+		}
+	}
+
 	private Label criarLabelTituloRotulo(String rotulo) {
 		return criarLabelTitulo(ProjetoMensagens.getString(rotulo), false);
+	}
+
+	private Label criarLabelTitulo(String chaveRotulo) {
+		Label label = new Label(ProjetoMensagens.getString(chaveRotulo), false);
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		return label;
 	}
 
 	private Label criarLabelTitulo(String rotulo, boolean chaveRotulo) {
--- src/br/com/persist/plugins/projeto/ProjetoPreferencia.java
+++ src/br/com/persist/plugins/projeto/ProjetoPreferencia.java
@@ -1,5 +1,6 @@
 package br.com.persist.plugins.projeto;
 
+import java.awt.Color;
 import java.util.prefs.Preferences;
 
 import javax.swing.SwingConstants;
@@ -9,6 +10,7 @@ import br.com.persist.formulario.Formulario;
 public class ProjetoPreferencia {
 	private static int projetoPosicaoAbaFichario;
 	private static boolean exibirArqIgnorados;
+	private static Color corElementoFinalRest;
 
 	private ProjetoPreferencia() {
 	}
@@ -17,12 +19,14 @@ public class ProjetoPreferencia {
 		Preferences pref = Preferences.userNodeForPackage(Formulario.class);
 		projetoPosicaoAbaFichario = pref.getInt("projeto_posicao_aba_fichario", SwingConstants.TOP);
 		exibirArqIgnorados = pref.getBoolean("projeto_exibir_arq_ignorados", false);
+		corElementoFinalRest = new Color(pref.getInt("cor_elemento_final_rest", Color.BLACK.getRGB()));
 	}
 
 	public static void salvar() {
 		Preferences pref = Preferences.userNodeForPackage(Formulario.class);
 		pref.putInt("projeto_posicao_aba_fichario", projetoPosicaoAbaFichario);
 		pref.putBoolean("projeto_exibir_arq_ignorados", exibirArqIgnorados);
+		pref.putInt("cor_elemento_final_rest", corElementoFinalRest.getRGB());
 	}
 
 	public static int getProjetoPosicaoAbaFichario() {
@@ -42,5 +46,13 @@ public class ProjetoPreferencia {
 
 	public static void setExibirArqIgnorados(boolean exibirArqIgnorados) {
 		ProjetoPreferencia.exibirArqIgnorados = exibirArqIgnorados;
+	}
+
+	public static Color getCorElementoFinalRest() {
+		return corElementoFinalRest;
+	}
+
+	public static void setCorElementoFinalRest(Color corElementoFinalRest) {
+		ProjetoPreferencia.corElementoFinalRest = corElementoFinalRest;
 	}
 }
